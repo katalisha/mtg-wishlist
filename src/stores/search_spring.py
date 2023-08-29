1582a0b94e7ce6674dcf2851bb7a832df58c701f
Kate <kate@katalisha.com>
2023-08-29 11:58:09 +1000

    Added additional checks for search spring stores

--- src/stores/search_spring.py
+++ src/stores/search_spring.py
@@ -42,11 +42,18 @@ class SearchSpringStore(Store):
     def perform_search_for_card(self, card: Card) -> Optional[StockedCard]:
         inventory = self.get_inventory(card)
 
-        if inventory.products:
-            min_price = min(p.price for p in inventory.products)
-            return StockedCard(card, min_price)
-        else:
+        if not inventory.products:
             return None
+
+        return self.validate_inventory(card, inventory)
+
+    def build_request_params(self, card: Card) -> dict[str, Any]:
+        return {
+            "siteId": self.site_id,
+            "bgfilter.collection_handle": "magic-the-gathering-singles",
+            "q": f"{card.number} {card.set_name}",
+            "resultsFormat": "native",
+        }
 
     def get_inventory(self, card: Card) -> Inventory:
         params = self.build_request_params(card)
@@ -63,13 +70,26 @@ class SearchSpringStore(Store):
             except httpx.HTTPError as exc:
                 raise CardSearchFailure from exc
 
-    def build_request_params(self, card: Card) -> dict[str, Any]:
-        return {
-            "siteId": self.site_id,
-            "bgfilter.collection_handle": "magic-the-gathering-singles",
-            "q": f"{card.number} {card.set_name}",
-            "resultsFormat": "native",
-        }
+    def validate_inventory(
+        self, card: Card, inventory: Inventory
+    ) -> Optional[StockedCard]:
+        def is_foil_product(product: Product) -> bool:
+            return product.collection_handle.count("foil") > 0
+
+        def product_matches_printing(product: Product, card: Card) -> bool:
+            return (
+                (card.printing == "Foil")
+                if (is_foil_product(product))
+                else (card.printing == "Normal")
+            )
+
+        try:
+            min_price = min(
+                p.price for p in inventory.products if product_matches_printing(p, card)
+            )
+            return StockedCard(card, min_price)
+        except ValueError:
+            return None
 
     def rate_limited_to(self) -> Optional[datetime]:
         return None
